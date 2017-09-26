cbdacdc90f558e1d6bc3717f88efd666c759f08a
Aakash <aakashno123@gmail.com>
2017-09-26 16:59:22 +0530

    Update Masking.java

--- Masking.java
+++ Masking.java
@@ -34,7 +34,8 @@ public static String decrypt(StringBuilder arg)
 	return s.toString();
 }
 public static void main(String []arg) {
-	Masking obj=new Masking();
+	Masking obj=new Masking();//project send kar or link bhi
+	
 	//String s=obj.encrypt("AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZ11223344556677889900aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz");
 	//System.out.println(s);
 	StringBuilder sb=new StringBuilder(obj.encrypt("AABBCCDDEEFFGGHHIIJJKKLLMMNNOOPPQQRRSSTTUUVVWWXXYYZZ11223344556677889900aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz"));
